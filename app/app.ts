8d9d1137c7476b09363bd15a89e44e28e6d3b25e
Jarmo Kukkola <jarmo.kukkola@gmail.com>
2016-07-28 11:27:32 +0200

    Translation works.

--- app/app.ts
+++ app/app.ts
@@ -2,21 +2,41 @@ import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
+import {TRANSLATE_PROVIDERS,TranslatePipe, TranslateService, TranslateLoader, TranslateStaticLoader} from 'ng2-translate/ng2-translate';
+import {Http,HTTP_PROVIDERS} from '@angular/http';
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  template: '<ion-nav [root]="rootPage"></ion-nav>',
+  providers: [
+    {
+      provide: TranslateLoader,
+      useFactory: (http: Http) => new TranslateStaticLoader(http, 'assets/i18n', '.json'),
+      deps: [Http]
+    },
+    TranslateService,
+  ]
 })
 export class MyApp {
 
   private rootPage: any;
 
-  constructor(private platform: Platform) {
+  constructor(private platform: Platform,
+    public translate: TranslateService) {
     this.rootPage = TabsPage;
 
     platform.ready().then(() => {
       StatusBar.styleDefault();
     });
+    this.translateConfig();
   }
+
+  translateConfig() {
+    var userLang = navigator.language.split('-')[0];
+    userLang = /(en|de)/gi.test(userLang) ? userLang : 'en';
+    this.translate.setDefaultLang('en');
+    this.translate.use(userLang);
+  }
+
 }
 
-ionicBootstrap(MyApp);
+ionicBootstrap(MyApp,[HTTP_PROVIDERS, TRANSLATE_PROVIDERS]);
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {HomePage} from '../home/home';
 import {AboutPage} from '../about/about';
 
+
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
 })
